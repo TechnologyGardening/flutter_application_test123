4ea53e2125480034a6f96d404d600d95401cb245
Technology Gardening <33565151+TechnologyGardening@users.noreply.github.com>
2025-07-01 13:15:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_application_test123
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_application_test123/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_application_test123
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
